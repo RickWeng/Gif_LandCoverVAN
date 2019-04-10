11e0fbe5b3d51f33e43b64de4ba1f7ef2b08b4c3
Ricky Weng <chosen1yi@gmail.com>
2019-04-10 13:53:18 -0700

    Update gif_ReclassifiedCity.R

--- gif_ReclassifiedCity.R
+++ gif_ReclassifiedCity.R
@@ -12,35 +12,35 @@ library(magick) # Make Gif
 # Set workspace
 setwd("C:/Ricky/Jenny/Reclassify_City")
 # List all raster files
-myfile = list.files(pattern="newVanrendvi_")
+myfile <- list.files(pattern = "newVanrendvi_")
 # Check files
 myfile
 # Set colors for each land cover class
-my_col=c("#FC8D62","#FFFFB3","#66C2A5")
+my_col <- c("#FC8D62", "#FFFFB3", "#66C2A5")
 
 # For loop to plot all maps
 for(rawfile in myfile){
   # Use different years as the title of each map and the name of each image file 
   original_name = paste0(rawfile)
-  change_name = gsub("newVanrendvi_|.tif","",original_name)
+  change_name = gsub("newVanrendvi_|.tif", "", original_name)
   print (change_name)
   # Save images
-  png(filename = paste0(change_name,".png"),width=800,height=600,res=150)
-  par(mar=c(0,4,0,0))
+  png(filename = paste0(change_name, ".png"), width = 800, height = 600, res = 150)
+  par(mar = c(0, 4, 0, 0))
   # Plot maps
-  plot(raster(rawfile),box=FALSE,axes=FALSE,legend=FALSE,col=my_col,main=change_name,adj=0,line = -1)
+  plot(raster(rawfile), box = FALSE, axes = FALSE, legend = FALSE, col = my_col, main = change_name, adj = 0, line = -1)
   # Put legend outside the box
-  par(xpd=TRUE)
-  legend("bottom",x.intersp=0.5,legend=c("Non-vegetated","Sparse vegetation","Dense vegetation"),fill=my_col,bty = "n",horiz = TRUE)
+  par(xpd = TRUE)
+  legend("bottom", x.intersp=0.5, legend=c("Non-vegetated", "Sparse vegetation", "Dense vegetation"), fill = my_col, bty = "n", horiz = TRUE)
   dev.off()
 }
 
 # Make Gif
-list.files(pattern=".png") %>%
+list.files(pattern = ".png") %>%
   map(image_read) %>%
   image_join() %>%
-  #Set frames per second to 2
-  image_animate(fps=4) %>%
+  #Set frames per second to 4
+  image_animate(fps = 4) %>%
   image_write("Landcover_cityVan.gif")
 
 
