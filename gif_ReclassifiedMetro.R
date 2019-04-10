821971816dc2346a96d5d93a63d442e192955b8c
Ricky Weng <chosen1yi@gmail.com>
2019-04-10 13:54:24 -0700

    Update gif_ReclassifiedMetro.R

--- gif_ReclassifiedMetro.R
+++ gif_ReclassifiedMetro.R
@@ -21,8 +21,8 @@ my_col <- c("#FC8D62", "#FFFFB3", "#66C2A5")
 # For loop to plot all maps
 for (rawfile in myfile) {
   # Use different years as the title of each map and the name of each image file 
-  original_name = paste0(rawfile)
-  change_name = gsub("newrendvi_|.tif", "", original_name)
+  original_name <- paste0(rawfile)
+  change_name <- gsub("newrendvi_|.tif", "", original_name)
   print (change_name)
   # Save images
   png(filename = paste0(change_name, ".png"), width = 800, height = 600, res = 150)
